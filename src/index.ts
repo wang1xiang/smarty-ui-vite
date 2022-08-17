3fc908c2ef1b2a118d0335cb4d3883f7d3ce484c
wangxiang <wangxiang@quickshopx.com>
2022-08-17 10:21:05 +0800

    学习基于 Vite 的组件库工程化实战：项目初始化及vite打包配置

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+// import SButton from './button';
+// import SButton from './button/SFCButton.vue'
+import SButton from './button/JSXButton'
+
+createApp(SButton).mount('#app')
