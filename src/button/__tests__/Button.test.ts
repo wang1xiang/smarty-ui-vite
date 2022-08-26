3bde111241d8e36f19df100d7b29eba81f80e38b
wangxiang <wangxiang@quickshopx.com>
2022-08-26 09:17:59 +0800

    eslint + prettier + husky

--- src/button/__tests__/Button.test.ts
+++ src/button/__tests__/Button.test.ts
@@ -2,7 +2,6 @@ import Button from "../Button";
 
 import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
-import { V } from "vitest/dist/global-fe52f84b";
 // 测试分组
 describe("Button", () => {
   // mount
@@ -18,41 +17,56 @@ describe("Button", () => {
     expect(wrapper.text()).toBe("Button");
   });
 });
-describe('color', () => {
-  test('default', () => {
+describe("color", () => {
+  test("default", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'Button'
-      }
-    })
-    console.log(wrapper.classes())
-    expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-blue-500')).toBe(true);
-  })
-  test('yellow', () => {
+        default: "Button",
+      },
+    });
+    console.log(wrapper.classes());
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-blue-500")
+    ).toBe(true);
+  });
+  test("yellow", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'RedButton',
+        default: "RedButton",
       },
       props: {
-        color: 'yellow'
-      }
-    })
-    expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-yellow-500')).toBe(true)
-  })
-})
+        color: "yellow",
+      },
+    });
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-yellow-500")
+    ).toBe(true);
+  });
+});
 
-describe('round', () => {
-  test('round', () => {
+describe("round", () => {
+  test("round", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'RoundButton',
+        default: "RoundButton",
       },
       props: {
-        round: true
-      }
-    })
-    expect(wrapper.classes().map(v => v.replace('\n', '')).includes("rounded-full")).toBe(true);
-  })
+        round: true,
+      },
+    });
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("rounded-full")
+    ).toBe(true);
+  });
   test("plain:true", () => {
     const wrapper = shallowMount(Button, {
       slots: {
@@ -63,7 +77,12 @@ describe('round', () => {
       },
     });
 
-    expect(wrapper.classes().map(v => v.replace('\n','')).includes("bg-blue-100")).toBe(true);
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-blue-100")
+    ).toBe(true);
   });
 });
 
@@ -77,7 +96,11 @@ test("icon", () => {
     },
   });
 
-  expect(wrapper.find("i").classes().map(v => v.replace('\n','')).includes("i-ic-baseline-edit")).toBe(
-    true
-  );
-})
+  expect(
+    wrapper
+      .find("i")
+      .classes()
+      .map((v) => v.replace("\n", ""))
+      .includes("i-ic-baseline-edit")
+  ).toBe(true);
+});
